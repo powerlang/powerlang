be8f94ef106e5d5e2d1577a44141a55d314fe56f
Javier Pimás <elpochodelagente@gmail.com>
2020-05-24 17:50:56 -0300

    doc: set master doc so that it builds in readthedocs.org

--- docs/conf.py
+++ docs/conf.py
@@ -30,6 +30,9 @@ release = '0.1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+master_doc = 'index'
+
+
 extensions = [
     "sphinx_rtd_theme"
 ]
