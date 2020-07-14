490d21ee4583daef8eb244e08bd82fa932b59d93
Javier Pimás <elpochodelagente@gmail.com>
2020-07-14 02:34:57 -0300

    bootstrap: cherrypick jan's commit to create instance of Kernel instead of Module and fill in Kernel extras (entry point). These are needed by the (C++) launcher to dispatch to the very first method. Shorten access to spec in SmalltalkBootstrapper through #'=>' message. Add temporary "fix" to float underprimitives (they generate an #int3 exception)

--- launcher/Windows/Memory.cpp
+++
@@ -1,9 +0,0 @@
-#include "Memory.h"
-
-uintptr_t AllocateMemory(uintptr_t base, uintptr_t size)
-{
-    return reinterpret_cast<uintptr_t>(VirtualAlloc((void*)base,
-                     pagealign(size), 
-                     MEM_COMMIT,
-                     PAGE_READWRITE));
-}
