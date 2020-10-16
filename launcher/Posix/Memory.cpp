d2c379b4cf3e81ce07f07d32e5ef87f9187aa9cd
Javier Pimás <elpochodelagente@gmail.com>
2020-10-16 02:59:33 -0300

    launcher: make memory allocated for image segments executable

--- launcher/Posix/Memory.cpp
+++ launcher/Posix/Memory.cpp
@@ -8,7 +8,7 @@ uintptr_t AllocateMemory(uintptr_t base, uintptr_t size)
 {
     return reinterpret_cast<uintptr_t>(mmap(reinterpret_cast<void*>(base),
                     pagealign(size),
-                    PROT_READ | PROT_WRITE,
+                    PROT_READ | PROT_WRITE | PROT_EXEC,
                     MAP_PRIVATE | MAP_ANONYMOUS,
                     0,
                     0));
