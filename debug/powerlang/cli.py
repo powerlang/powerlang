039e9b616b889db5c5bc3b610ccdfdbc50ba3301
Jan Vrany <jan.vrany@labware.com>
2020-10-29 10:26:56 +0000

    debug: add new command `am` to disassemble a method
    Signed-off-by: Jan Vrany <jan.vrany@labware.com>

--- debug/powerlang/cli.py
+++ debug/powerlang/cli.py
@@ -60,6 +60,30 @@ class __DumpObjectCmd(gdb.Command):
 
 do = __DumpObjectCmd('do', gdb.COMMAND_DATA)
 
+def _lookup_symbol(pc_or_regexp):
+    pc = None
+    regexp = None
+    try:
+        pc = None
+        if isinstance(pc_or_regexp, int):
+            pc = pc_or_regexp
+        else:
+            pc = gdb.parse_and_eval(pc_or_regexp)
+    except:
+        regexp = re.compile(pc_or_regexp)
+
+    symtabs = (segment.symtab for segment in segments)
+
+    if pc != None:    
+        for symtab in symtabs:
+            sym = symtab.lookup_symbol_by_addr(pc)
+            if sym != None:
+                return [sym]
+        return []
+    if regexp != None:
+        return (sym for sym in flatten(symtabs) if regexp.search(sym.name))
+
+
 class __LookupMethod(gdb.Command):
     """
     Looks up a method containing given PC or matching given REGEXP.
@@ -83,28 +107,52 @@ class __LookupMethod(gdb.Command):
             print(method)
 
     def __call__(self, pc_or_regexp = '$pc'):
-        pc = None
-        regexp = None
-        try:
-            pc = None
-            if isinstance(pc_or_regexp, int):
-                pc = pc_or_regexp
-            else:
-                pc = gdb.parse_and_eval(pc_or_regexp)
-        except:
-            regexp = re.compile(pc_or_regexp)
-
-        symtabs = (segment.symtab for segment in segments)
-
-        if pc != None:    
-            for symtab in symtabs:
-                sym = symtab.lookup_symbol_by_addr(pc)
-                if sym != None:
-                    return [sym.method]
-            return []
-        if regexp != None:
-            return (sym.method for sym in flatten(symtabs) if regexp.search(sym.name))
-
+        return (sym.method for sym in _lookup_symbol(pc_or_regexp))
+        
 lm = __LookupMethod('lm', gdb.COMMAND_DATA)
 
+class __DisassembleMethod(gdb.Command):
+    """
+    Disassemble a method with given PC or matching given REGEXP.
+    Usage: lm [PC]
+           lm REGEXP
+    
+    PC can be given as an expression evaluating to an address
+    or omitted in which case value if $pc (PC of currently 
+    selected frame) is used. 
 
+    If REGEXP is given then method matching that REGEXP is
+    disassembled
+    """
+    def invoke(self, args, from_tty):        
+        argv = gdb.string_to_argv(args)
+        if len(argv) > 1:
+            raise Exception("lm takes only one argument (%d given)" % len(argv))
+        elif len(argv) == 0:
+            argv = ['$pc']    
+        self(argv[0])
+
+    def __call__(self, pc_or_regexp = '$pc'):          
+        methodsyms = list(_lookup_symbol(pc_or_regexp))        
+        if len(methodsyms) == 0:
+            print("No method matching %s" % pc_or_regexp)
+        elif len(methodsyms) > 1: 
+            print("Multiple methods matching %s:" % pc_or_regexp)
+            for methodsym in methodsyms:
+                print(methodsym)
+            print("Please disambiguate")
+        else:
+            self.disassemble(methodsyms[0])
+
+
+    def disassemble(self, methodsym):
+        arch = gdb.selected_inferior().architecture()
+        loPC = methodsym.address
+        hiPC = loPC + methodsym.size
+
+        print(methodsym)
+        for insn in arch.disassemble(loPC, hiPC - 1):            
+            print("  0x%016x: %s" % ( insn['addr'], insn['asm'] ));            
+
+
+am = __DisassembleMethod('am', gdb.COMMAND_DATA)
