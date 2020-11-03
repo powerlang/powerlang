22eac932107aef21fb6b256145629a8e365b959a
Javier Pimás <elpochodelagente@gmail.com>
2020-11-03 00:25:52 -0300

    bootstrap: correct configuration of write barrier and lookup nativizer, use a new nativization environment on each to avoid aliasing problems.

--- launcher/Windows/Memory.cpp
+++ launcher/Windows/Memory.cpp
@@ -4,6 +4,6 @@ uintptr_t AllocateMemory(uintptr_t base, uintptr_t size)
 {
     return reinterpret_cast<uintptr_t>(VirtualAlloc((void*)base,
                      pagealign(size), 
-                     MEM_COMMIT | MEM_RESERVE,
-                     PAGE_EXECUTE_READWRITE));
+                     MEM_COMMIT,
+                     PAGE_READWRITE));
 }
